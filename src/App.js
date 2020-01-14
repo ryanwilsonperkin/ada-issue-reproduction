c69f43ee589e338b1e5a033b41ad54e65206ba5f
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-01-14 13:16:44 -0500

    Use data-lazy and start Ada programatically

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import React from "react";
 
 function App() {
   const [adaLoaded, setAdaLoaded] = React.useState(false);
+  const [name, setName] = React.useState("ryan");
+  const [email, setEmail] = React.useState("ryan@example.com");
 
   // Wait until adaReady resolves before using Ada
   React.useEffect(() => {
+    window.adaReady.then(() => setAdaLoaded(true));
+  }, []);
+
+  // Call start once ready and also whenever metaFields change
+  React.useEffect(() => {
     window.adaReady.then(() => {
-      const email = "ryan@example.com";
       try {
-        window.adaEmbed.setMetaFields({ email });
+        window.adaEmbed.start({
+          metaFields: { email, name }
+        });
       } catch (error) {
         console.error(error);
       }
-      setAdaLoaded(true)
     });
-  }, []);
+  }, [name, email]);
+
+  const onChangeEmail = event => setEmail(event.target.value);
+  const onChangeName = event => setName(event.target.value);
 
   return (
     <div className="App">
-      <h1>Is Ada loaded?</h1>
-      <h2>{adaLoaded ? "Yes" : "No"}</h2>
+      <h1>Ada Test</h1>
+      <p>Ada is loaded: {adaLoaded ? "Yes" : "No"}</p>
+      <h2>Meta Fields</h2>
+      <form>
+        <label>
+          Name:
+          <input name="name" type="text" value={name} onChange={onChangeName} />
+        </label>
+        <label>
+          Email:
+          <input
+            name="name"
+            type="text"
+            value={email}
+            onChange={onChangeEmail}
+          />
+        </label>
+      </form>
     </div>
   );
 }
